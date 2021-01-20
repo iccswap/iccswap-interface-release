53de0bfcf5633f5d673d0a8c8ab32055e54011a6
icncde <common@icctoro.com>
2021-01-20 17:44:57 +0800

    2021-1-20:更新链接url

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a5f29be1ddfb5d73059b587c894506b8.js"
+  "./precache-manifest.a15501e67590df1eb48fbd6d7551b140.js"
 );
 
 self.addEventListener('message', (event) => {
